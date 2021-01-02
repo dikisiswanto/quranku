a095b0a6220a6d47ca3fac06fbf1856eeae494ad
Diki Siswanto <dickyachmady@gmail.com>
2021-01-02 12:34:13 +0800

    chore(optimization): add runtimeCaching strategy

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    runtimeCaching: {
+      urlPattern: /\/surah\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'surah-resources',
+      },
+    },
   },
 });
